6e7d7b7717e7d6beabe2df6d14349a87e4142153
MDecker-MobileComputing <mide_42b@yahoo.de>
2021-12-09 19:52:16 +0100

    Update prozenztwert.pipe.ts

--- src/app/pipe/prozenztwert.pipe.ts
+++ src/app/pipe/prozenztwert.pipe.ts
@@ -10,7 +10,6 @@ export class ProzentwertPipe implements PipeTransform {
     const prozentwert = value*100.0 / grundwert;
 
     const faktor = Math.pow(10, nachkommastellen);
-
     const prozentwertGerundet = Math.round(prozentwert*faktor) / faktor;
 
     return prozentwertGerundet + "%";
